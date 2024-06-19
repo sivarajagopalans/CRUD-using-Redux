1f4932241bc0ba16539eb10f6600b92ae041b186
sivarajagopalans <sivarajagopalans@gmail.com>
2024-06-19 19:07:58 +0530

    create reducers

--- src/slices/tasksSlice.jsx
+++ src/slices/tasksSlice.jsx
@@ -1,15 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const initialState={
-    taskList:[],
-    selectedTask:{}
+const initialState = {
+    taskList: [],
+    selectedTask: {}
 }
-const taskSlice=createSlice({
-    name:'taskSlice',
+const taskSlice = createSlice({
+    name: 'taskSlice',
     initialState,
-    reducers:{
-        
+    reducers: {
+
+        addTaskToList: (state, action) => {
+            let id = Math.random() * 100;
+            let task = { ...action.payload, id };
+            state.taskList.push(task);
+        },
+
+        removeTaskFromList: (state, action) => {
+            state.taskList = state.taskList.filter((task) => { task.id !== action.payload.id })
+        },
+
+        updateTaskInList: (state, action) => {
+            state.taskList = state.taskList.map((task) => { task.id === action.payload.id ? action.payload : task })
+        },
+
+        setSelectedTask: (state, action) => {
+            state.selectedTask = action.payload;
+        }
     }
 
 })
+
+
+export const { addTaskToList, removeTaskFromList, updateTaskInList, setSelectedTask } = taskSlice.actions;
+
+export default taskSlice.reducer;
