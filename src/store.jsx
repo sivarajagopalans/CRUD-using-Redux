134ff17ee6b9e63984db3de96ced6883a3f43778
sivarajagopalans <sivarajagopalans@gmail.com>
2024-06-19 19:22:58 +0530

    update reducer in store.js

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from './slices/tasksSlice'
+
+
 
 export const store =configureStore({
     reducer:{
-        
+        tasks:taskReducer
     }
 })
