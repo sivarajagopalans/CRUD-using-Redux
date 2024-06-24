adabf9940bd61c46fcdd5428b0011560dc73ce2f
sivarajagopalans <sivarajagopalans@gmail.com>
2024-06-24 18:34:24 +0530

    create read task using useSelector

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import { useState } from 'react'
 import { addTaskToList } from '../slices/tasksSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 export const AddTask = () => {
 
-    const dispatch=useDispatch();
+    const dispatch = useDispatch();
+    const {tasksList}=useSelector((state)=> state.tasks)
 
     const [task, setTask] = useState('');
     const [taskDesc, setTaskDesc] = useState('');
-  
+
     const addTask = (e) => {
-      e.preventDefault();
-      console.log({task,taskDesc});
-      dispatch(addTaskToList({task,taskDesc}))
+        e.preventDefault();
+        console.log({ task, taskDesc });
+        dispatch(addTaskToList({ task, taskDesc }))
     }
 
     return (
         <>
 
-           <h1 className='heading'>CRUD OPERATION</h1>
-           <p className='tasks-count'>Currently 0 task(s) pending</p>
+            <h1 className='heading'>CRUD OPERATION</h1>
+            <p className='tasks-count'>{`Currently ${tasksList.length} task(s) pending`}</p>
             <div className="form-group">
                 <div className="task-name">
                     <label htmlFor="task name">Task Name</label>
                     <input type="text" placeholder='enter task name' value={task}
-                        onChange={(e) => { setTask(e.target.value) }} required/>
+                        onChange={(e) => { setTask(e.target.value) }} required />
                 </div>
                 <div className="task-desc">
                     <label htmlFor="task sesc">Task Description</label>
