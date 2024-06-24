d22aa54d84043ad4437fecd67c77d448401aef04
sivarajagopalans <sivarajagopalans@gmail.com>
2024-06-24 18:28:09 +0530

    create add task using useDispatch

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import { useState } from 'react'
+import { addTaskToList } from '../slices/tasksSlice';
+import { useDispatch } from 'react-redux';
 
 
 export const AddTask = () => {
 
+    const dispatch=useDispatch();
+
     const [task, setTask] = useState('');
     const [taskDesc, setTaskDesc] = useState('');
   
-    const handleClick = (e) => {
+    const addTask = (e) => {
       e.preventDefault();
-      console.log(task,",", taskDesc);
+      console.log({task,taskDesc});
+      dispatch(addTaskToList({task,taskDesc}))
     }
 
     return (
         <>
 
-            <h1>CRUD OPERATION</h1>
+           <h1 className='heading'>CRUD OPERATION</h1>
+           <p className='tasks-count'>Currently 0 task(s) pending</p>
             <div className="form-group">
                 <div className="task-name">
                     <label htmlFor="task name">Task Name</label>
                     <input type="text" placeholder='enter task name' value={task}
-                        onChange={(e) => { setTask(e.target.value) }} required />
+                        onChange={(e) => { setTask(e.target.value) }} required/>
                 </div>
                 <div className="task-desc">
                     <label htmlFor="task sesc">Task Description</label>
                     <input type="text" placeholder='enter task description' value={taskDesc}
                         onChange={(e) => { setTaskDesc(e.target.value) }} required />
                 </div>
-                <button type='button' onClick={handleClick}>Add</button>
+                <button type='button' onClick={addTask}>Add</button>
             </div>
 
         </>
