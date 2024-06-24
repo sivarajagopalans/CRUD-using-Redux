a39391ec8b6c3c6fe410a8af0cde30507afbb870
sivarajagopalans <sivarajagopalans@gmail.com>
2024-06-24 18:38:52 +0530

    modify task table

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,36 +2,38 @@ import React, { useState } from 'react'
 import { UpdateTasks } from './UpdateTasks';
 
 export const TaskList = () => {
-    const [updateTaskBtn,setUpdateTaskBtn]=useState(false);
-    const updateTask=()=>{
+    const [updateTaskBtn, setUpdateTaskBtn] = useState(false);
+    const updateTask = () => {
         setUpdateTaskBtn(true);
     }
-    const deleteTask=()=>{
+    const deleteTask = () => {
         console.log("deleteTask");
     }
-  return (
-    <div className='Table'>
-        <table>
-            <thead>
-                <tr>
-                    <th>Task</th>
-                    <th>Desciption</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Write code</td> 
-                    <td className='action-btn'>
-                        <button onClick={()=>{updateTask()}}>🖌</button> 
-                        <button onClick={()=>{deleteTask()}}>🗑</button></td>
-                </tr>
-            </tbody>
-        </table>
-        {updateTaskBtn &&  <div className='box-transform'>
-                <UpdateTasks close={setUpdateTaskBtn}/>
+    return (
+        <div className='Table'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Task</th>
+                        <th>Desciption</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>code</td>
+                        <td>Write code</td>
+                        <td className='action-btn'>
+                            <button onClick={() => { updateTask() }}>🖌</button>
+                            <button onClick={() => { deleteTask() }}>🗑</button></td>
+                    </tr>
+                </tbody>
+            </table>
+            {updateTaskBtn && <div className='box-transform'>
+                <UpdateTasks close={setUpdateTaskBtn} />
             </div>}
-    </div>
-  )
+        </div>
+    )
 }
